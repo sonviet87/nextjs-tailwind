540b6e4c6f7250f192be74adc1f29702cda3a1fb
devel02 <devel02@iconsys84.com>
2023-08-01 15:43:47 +0700

    update

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -2,10 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Link from "next/link";
 
-MenuItem.propTypes = {
-
-};
-
 function MenuItem({title,address,Icon}) {
     return (
         <div>
